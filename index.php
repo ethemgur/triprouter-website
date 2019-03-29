8713e6bc8b10a7391e40295742b44c05b68cd7a7
Ethem Gür <iethemgur@gmail.com>
2019-03-29 22:29:54 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="img/fav.png">
+	<!-- Author Meta -->
+	<meta name="author" content="codepixer">
+	<!-- Meta Description -->
+	<meta name="description" content="The Perfect Trip Experience For You">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="trip, route, travel, router">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>TripRouter | The Perfect Trip For You</title>
+	
+	<link rel='icon' href='TR_logo.png'>
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,600|Roboto:400,400i,500" rel="stylesheet">
+	<!--
+			CSS
+			============================================= -->
+	<link rel="stylesheet" href="css/linearicons.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/nice-select.css">
+	<link rel="stylesheet" href="css/hexagons.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+	<!-- start header Area -->
+	<header id="header">
+		<div class="container main-menu" >
+			<div class="row align-items-center justify-content-between d-flex">
+				<div id="logo">
+					<a href="index.html"><img id='logo-img' src="TR_logo.png" alt="" title="" style="width:100px;height:100px;"/></a>
+				</div>
+				<nav id="nav-menu-container" style="text-emphasis-color: black">
+					<ul class="nav-menu">
+						<li class="menu-active"><a href="#">Home</a></li>
+						<li class="menu-active"><a href="#why">Why TripRouter?</a></li>
+						<li><a href="#contact">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<!-- end header Area -->
+
+	<!-- start banner Area -->
+	<section class="home-banner-area">
+		<div class="container">
+            <br>
+            <br>
+            <br>
+			<div class="row fullscreen d-flex align-items-center justify-content-between">
+                
+				<div class="home-banner-content col-lg-6 col-md-6">
+					<h1 id="about">
+						Take The Perfect Trip For Your Budget!
+					</h1>
+					<p>TripRouter creates the best route for you to take into account your budget and your holiday habits.</p>
+					<div class="download-button d-flex flex-row justify-content-start">
+						<div class="buttons flex-row d-flex">
+							<i class="fa fa-apple" aria-hidden="true"></i>
+							<div class="desc">
+									<p style="text-align: center">
+										<span>Coming Soon</span>
+										<br>
+										on App Store
+									</p>
+							</div>
+						</div>
+						<div class="buttons dark flex-row d-flex">
+							<i class="fa fa-android" aria-hidden="true"></i>
+							<div class="desc">
+									<p style="text-align: center">
+										<span>Coming Soon</span>
+										<br>
+										on Google Play 
+									</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="banner-img col-lg-4 col-md-6">
+					<img class="img-fluid" src="img/mock-my-trips.png" alt="" style="width: 290px ;height: 580px;">
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End banner Area -->
+
+	<!-- Start feature Area -->
+	<section class="feature-area section-gap-top">
+		<div class="container">
+			<div class="row" id="why">
+				<div class="col-lg-4 col-md-6">
+					<div class="single-feature">
+						<a href="#" class="title">
+							<span class="lnr lnr-diamond"></span>
+							<h3>Individualized</h3>
+						</a>
+						<p>
+							A unique tour plan according to your preferences. No need for fixed plans of agencies.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="single-feature">
+						<a href="#" class="title">
+							<span class="lnr lnr-hourglass"></span>
+							<h3>Save Time</h3>
+						</a>
+						<p>
+							Don't spend too much time on useless websites. TripRouter finds the best route for you in a few seconds.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="single-feature">
+						<a href="#" class="title">
+							<span class="lnr lnr-rocket"></span>
+							<h3>Easy to Use</h3>
+						</a>
+						<p>
+							Your best route is a few clicks away. No complicated design or procedure.
+						</p>
+						<br>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	<!-- End feature Area -->
+
+	<!-- Start about Area -->
+	<section class="about-area">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-5 home-about-left">
+					<img class="img-fluid" src="img/mock-categories.png" alt="" style="width: 290px ;height: 580px;">
+				</div>
+				<div class="offset-lg-1 col-lg-5 home-about-right">
+					<h2 style="align-content: center; text-align: center">
+						All you have to do is determine your preferences and vacation destination related to your holiday! 
+					</h2>
+				</div>
+				<div class="col-lg-6 home-about-right home-about-right2">
+					<h2 style="align-content: center; text-align: center">
+						Rest of the work takes just a few seconds for TripRouter's artificial intelligence-based algorithm!
+					</h2>
+				</div>
+				<div class="col-lg-5 home-about-left" style="text-align: right">
+					<img class="img-fluid" src="img/mock-map.png" alt="" style="width: 290px ;height: 580px;">
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End about Area -->
+
+	<!-- Start Footer Area -->
+	<footer class="footer-area section-gap" id='contact'>
+		<div class="container">
+			<div class='row'>
+				<div class="col-lg-12">
+					<form class="form-area " id="myForm" action="mail.php" method="post" class="contact-form text-right">
+						<div class="row">
+							<div class="col-lg-4 form-group">
+								<input name="name" placeholder="Enter your name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'"
+								 class="common-input mb-20 form-control" required="" type="text">
+
+								<input name="email" placeholder="Enter email address" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
+								 onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" class="common-input mb-20 form-control"
+								 required="" type="email">
+
+								<input name="subject" placeholder="Enter your subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your subject'"
+								 class="common-input mb-20 form-control" required="" type="text">
+							</div>
+							<div class="col-lg-8 form-group">
+								<textarea class="common-textarea form-control" name="message" placeholder="Message" onfocus="this.placeholder = ''"
+								 onblur="this.placeholder = 'Message'" required=""></textarea>
+							</div>
+							<div class="col-lg-12 d-flex justify-content-between">
+								<div class="alert-msg" style="text-align: left;"></div>
+								<button class="primary-btn" style="float: right;">Send Message</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="footer-bottom row align-items-center">
+				<p class="footer-text m-0 col-lg-6 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+				<div class="col-lg-6 col-md-6 social-link">
+					<div class="download-button d-flex flex-row justify-content-end">
+						<div class="buttons gray flex-row d-flex">
+							<i class="fa fa-apple" aria-hidden="true"></i>
+							<div class="desc">
+									<p style="text-align: center">
+										<span>Coming Soon</span> <br>
+										on App Store
+									</p>
+							</div>
+						</div>
+						<div class="buttons gray flex-row d-flex">
+							<i class="fa fa-android" aria-hidden="true"></i>
+							<div class="desc">
+									<p style="text-align: center">
+										<span>Coming Soon</span> <br>
+										on Play Store
+									</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- End Footer Area -->
+
+	<script src="js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+	 crossorigin="anonymous"></script>
+	<script src="js/tilt.jquery.min.js"></script>
+	<script src="js/vendor/bootstrap.min.js"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+	<script src="js/easing.min.js"></script>
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.min.js"></script>
+	<script src="js/jquery.ajaxchimp.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/owl-carousel-thumb.min.js"></script>
+	<script src="js/hexagons.min.js"></script>
+	<script src="js/jquery.nice-select.min.js"></script>
+	<script src="js/waypoints.min.js"></script>
+	<script src="js/mail-script.js"></script>
+	<script src="js/main.js"></script>
+</body>
+
+</html>
